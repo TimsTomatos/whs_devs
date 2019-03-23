5a4519d68376aeb6e656a48cbc305ab788fbd1bf
TimsTomatos <30964932+TimsTomatos@users.noreply.github.com>
2019-03-22 20:06:57 -1000

    Finished the Login authetication functionalty, needs styling... Like a lit

--- apps/auth.js
+++ apps/auth.js
@@ -1,17 +1,17 @@
-// Sign Up
+// Sign Up Variables
 const user = document.querySelector("#user");
 const pass = document.querySelector("#pass");
 const form = document.querySelector("#form");
 
-// Login Up 
-const logIn = document.querySelector("#logIn");
-const user_login = document.querySelector();
-const pass_login = document.querySelector();
+// Login Up Variables
+const log_in = document.querySelector("#logIn");
+const user_login = document.querySelector("#username");
+const pass_login = document.querySelector("#password");
+
+var render_username = document.querySelector("#render_username"); 
 
 
-
-console.log("Hello", form);
-
+// Sign Up Functions 
 form.addEventListener('submit', function(parm) {
     parm.preventDefault(); // Prevents Page Form refreshing
     console.log("Neato Libudi")
@@ -23,20 +23,39 @@ form.addEventListener('submit', function(parm) {
     })
 });
 
-logIn.addEventListener('submit', function(parm) {
-    parm.preventDefault();
-    var 
-})
+
 auth.onAuthStateChanged(function(user) { // Checks if they are signed in or signed out
     if (user)
     {
         console.log("Logged in");
-        displayUsername.innerHTML = user.email;
+        render_username.innerHTML = user.email;
     }
     else 
     {
         console.log("Logged out");
-        signInStuff.forEach(e => e.style.display="none");
-        signOutStuff.forEach(e => e.style.display="block");
     }
 });
+
+
+// Login Stuff Functions
+login.addEventListener('submit', function(parm) {
+    parm.preventDefault();
+    
+    var username_value = user_login.value;
+    var password_value =  pass_login.value;
+
+    auth.signInWithEmailAndPassword(username_value, password_value)
+    .then(function () {
+        console.log("Login Successful")
+    })
+})
+
+// logout
+logout_button.addEventListener('click', function () {
+    auth.signOut()
+    .then(function() {
+        console.log("logged out")
+        render_username.innerHTML = null;
+    })
+
+})
