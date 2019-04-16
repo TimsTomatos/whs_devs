f4cd4a83e0943d132fc03ea3b3dd694112c40c92
SilverNeko21 <larong2122@gmail.com>
2019-04-15 14:16:25 -1000

    needs some tweeks and footer

--- apps/homePage2.js
+++ apps/homePage2.js
@@ -12,3 +12,40 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+//var images = new Array();
+var container = document.getElementById("img-container");
+var increment = 0;
+var images = [
+    '../images/image2.jpg',
+    '/images/image1.jpg',
+    '/images/image3.jpg'
+]
+
+// images[0] = new Image();               //Defines Array index value
+// images[0].src = '/images/image1.jpg'; //Assigns value to array index
+
+// images[1] = new Image(); 
+// images[1].src = '/images/image2.jpg';
+
+// images[2] = new Image();
+// images[2].src = '/images/image3.jpg';
+
+function rightImage() {
+    if (increment == 2) {
+        increment = 0;
+    }
+    increment++;
+    container.style.backgroundImage = "url(images[increment])"; 
+    console.log(increment)
+    console.log(container.style.backgroundImage.src)
+}
+
+function leftImage() {
+    if (increment == 0) {
+        increment = 3;
+    }
+    increment--;
+    container.style.backgroundImage = images[increment]; 
+    console.log(increment)
+}
