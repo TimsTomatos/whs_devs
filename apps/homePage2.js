be4a2caf61584685a2024c9e9582c77b565aaec6
TimsTomatos <30964932+TimsTomatos@users.noreply.github.com>
2019-04-21 22:46:34 -1000

    Login Stuff on the new homepage DONE

--- apps/homePage2.js
+++ apps/homePage2.js
@@ -49,3 +49,124 @@ function leftImage() {
     container.style.backgroundImage = images[increment]; 
     console.log(increment)
 }
+
+
+
+// Modal Stuff
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("login/join-button");
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
+// Lpgin Box Switch
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+        this.parentElement.classList.add('is-active')
+        console.log("oof")
+	})
+})
+
+
+// Authetication Stuff
+// Sign Up Variables
+const user = document.querySelector("#user");
+const pass = document.querySelector("#pass");
+const form = document.querySelector("#form");
+const signup_button = document.querySelector("#signup-button");
+
+
+// Login Up Variables
+const log_in = document.querySelector("#logIn");
+const user_login = document.querySelector("#login-email");
+const pass_login = document.querySelector("#login-password");
+const login_button = document.querySelector("#login-button")
+var render_username = document.querySelector("#render-username"); 
+
+
+//Logout Button
+const logout_button = document.querySelector("#logout-button");
+
+// Sign Up Functions 
+signup_button.addEventListener('submit', function(parm) {
+    parm.preventDefault(); // Prevents Page Form refreshing
+    console.log("Neato Libudi")
+    var user_value = user.value;
+    var pass_value = pass.value;
+    auth.createUserWithEmailAndPassword(user_value, pass_value)
+    .then(function() {
+        console.log("Sign up successful")
+    })
+});
+
+
+auth.onAuthStateChanged(function(user) { // Checks if they are signed in or signed out
+    if (user)
+    {
+        console.log("Logged in");
+        logout_button.style.display = "block";
+        render_username.style.display = "block";
+        render_username.innerHTML = user.email;
+    }
+    else 
+    {
+        console.log("Logged out");
+    }
+});
+
+
+// Login Stuff Functions
+login_button.addEventListener('click', function(parm) {
+    parm.preventDefault();
+    
+    var username_value = user_login.value;
+    var password_value =  pass_login.value;
+
+    auth.signInWithEmailAndPassword(username_value, password_value)
+    .then(function (user) {
+        
+        //Checks for admin account
+        if (user = "admin@gmail.com") {
+            console.log("Working as Intended!!") 
+        }
+        console.log("Login Successful")
+    })
+})
+login_button.onclick = function() {
+    modal.style.display = "none";
+  }
+
+
+// logout
+logout_button.addEventListener('click', function () {
+    auth.signOut()
+    .then(function() {
+        console.log("logged out")
+        render_username.innerHTML = null;
+    })
+
+})
+
+
+
+
+
+
