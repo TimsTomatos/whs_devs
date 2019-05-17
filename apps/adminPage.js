1967ed82ddfbea17b87372468065ba25cc4c92e5
caslabs <casourcer@gmail.com>
2019-05-17 10:13:06 -1000

    admin updates

--- apps/adminPage.js
+++ apps/adminPage.js
@@ -1,56 +1,53 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var cta = document.getElementById('thisChart').getContext('2d');
+var data = {
+    // A labels array that can contain any sort of values
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+      [5, 2, 4, 2, 0]
+    ]
+  };
+  
 
-var myLineChart = new Chart(cta, {
-  type: 'line',
-  data: [0.1, 0.2, 0.3, 5, 2, 3],
-  options: {
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
-        }]
+var data2 = {
+    labels: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+    series: [20, 60, 120, 200, 180, 20, 10]
+  }
+
+
+  // As options we currently only set a static size of 300x200 px. We can also omit this and use aspect ratio containers
+  // as you saw in the previous example
+  var options = {
+    distributeSeries: true
+  };
+  
+  // Create a new line chart object where as first parameter we pass in a selector
+  // that is resolving to our chart container element. The Second parameter
+  // is the actual data object. As a third parameter we pass in our custom options.
+  new Chartist.Line('#linePlot', data, options);
+  new Chartist.Bar('#compBar', data2, options);
+
+
+  
+  new Chartist.Bar('#quaterBar', {
+    labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+    series: [
+      [800000, 1200000, 1400000, 1300000],
+      [200000, 400000, 500000, 300000],
+      [100000, 200000, 400000, 600000]
+    ]
+  }, {
+    stackBars: true,
+    axisY: {
+      labelInterpolationFnc: function(value) {
+        return (value / 1000) + 'k';
+      }
     }
-}
-});
-
-
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Mcdonalds', 'Caslabs', 'Taco Bell', 'YMCA', 'Aloha Market', 'Walmart'],
-        datasets: [{
-            label: 'Current Donation Amount',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'purple',
-                'blue',
-                'red',
-                'yellow',
-                'green',
-                'orange'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+  }).on('draw', function(data) {
+    if(data.type === 'bar') {
+      data.element.attr({
+        style: 'stroke-width: 30px'
+      });
     }
-});
+  });
 
-
+  
