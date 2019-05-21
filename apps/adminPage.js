706db0402f6c714a34a42e16edb860e23a51700a
caslabs <casourcer@gmail.com>
2019-05-21 01:45:51 -1000

    admin introduction modal

--- apps/adminPage.js
+++ apps/adminPage.js
@@ -1,59 +1,69 @@
 var data = {
-    // A labels array that can contain any sort of values
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-    // Our series array that contains series objects or in this case series data arrays
-    series: [
-      [5, 2, 4, 2, 0]
-    ]
-  };
-  
+  // A labels array that can contain any sort of values
+  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+  // Our series array that contains series objects or in this case series data arrays
+  series: [
+    [5, 2, 4, 2, 0]
+  ]
+};
+
 
 var data2 = {
-    labels: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-    series: [20, 60, 120, 200, 180, 20, 10]
+  labels: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+  series: [20, 60, 120, 200, 180, 20, 10]
+}
+
+
+// As options we currently only set a static size of 300x200 px. We can also omit this and use aspect ratio containers
+// as you saw in the previous example
+var options = {
+  distributeSeries: true
+};
+
+// Create a new line chart object where as first parameter we pass in a selector
+// that is resolving to our chart container element. The Second parameter
+// is the actual data object. As a third parameter we pass in our custom options.
+new Chartist.Line('#linePlot', data, options);
+new Chartist.Bar('#compBar', data2, options);
+
+
+
+new Chartist.Bar('#quaterBar', {
+  labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+  series: [
+    [800000, 1200000, 1400000, 1300000],
+    [200000, 400000, 500000, 300000],
+    [100000, 200000, 400000, 600000]
+  ]
+}, {
+  stackBars: true,
+  axisY: {
+    labelInterpolationFnc: function (value) {
+      return (value / 1000) + 'k';
+    }
   }
+}).on('draw', function (data) {
+  if (data.type === 'bar') {
+    data.element.attr({
+      style: 'stroke-width: 30px'
+    });
+  }
+});
 
 
-  // As options we currently only set a static size of 300x200 px. We can also omit this and use aspect ratio containers
-  // as you saw in the previous example
-  var options = {
-    distributeSeries: true
-  };
-  
-  // Create a new line chart object where as first parameter we pass in a selector
-  // that is resolving to our chart container element. The Second parameter
-  // is the actual data object. As a third parameter we pass in our custom options.
-  new Chartist.Line('#linePlot', data, options);
-  new Chartist.Bar('#compBar', data2, options);
 
+var elem = document.querySelector('.modal');
+var instance = M.Modal.init(elem);
+instance.open();
 
-  
-  new Chartist.Bar('#quaterBar', {
-    labels: ['Q1', 'Q2', 'Q3', 'Q4'],
-    series: [
-      [800000, 1200000, 1400000, 1300000],
-      [200000, 400000, 500000, 300000],
-      [100000, 200000, 400000, 600000]
-    ]
-  }, {
-    stackBars: true,
-    axisY: {
-      labelInterpolationFnc: function(value) {
-        return (value / 1000) + 'k';
-      }
-    }
-  }).on('draw', function(data) {
-    if(data.type === 'bar') {
-      data.element.attr({
-        style: 'stroke-width: 30px'
-      });
-    }
-  });
+$(document).ready(function(){
+  $('.carousel').carousel();
+
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  }, elems);
 
   
-
-  var elem= document.querySelector('.modal');
-  var instance = M.Modal.init(elem);
-  instance.open();
-
-         
+});
